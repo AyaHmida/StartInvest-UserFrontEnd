83d020fca2e17a0cc495bbfc1e5539edcbfc78d9
chayma bounawara <mostafachaima90@gmail.com>
2024-03-18 13:34:40 +0100

    corriger type

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,10 +1,62 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { callApi } from "../api";
 
 function Login() {
-  const { search } = useLocation();
-  const errorMessage = new URLSearchParams(search).get("message");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSumit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    if (!email || !password) {
+      setError("Veuillez remplir tous les champs.");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const data = await callApi("auth/login", "POST", { email, password });
+
+      localStorage.setItem("token", data.access_token);
+
+      const userType = await getUserType();
+      if (userType === "fondateur" || userType === "investisseur") {
+        navigate("/publication");
+      } else {
+        navigate("/updateProfile");
+      }
+    } catch (err) {
+      console.error(err);
+      setError(
+        err.response?.data?.message ||
+          "Une erreur s'est produite. Veuillez réessayer."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getUserType = async () => {
+    try {
+      // Appeler l'API pour récupérer le type d'utilisateur
+      const response = await callApi("auth/user", "GET", null, {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      });
+      return response.type;
+    } catch (error) {
+      console.error(
+        "Erreur lors de la récupération du type d'utilisateur:",
+        error
+      );
+      throw error;
+    }
+  };
 
   const handleGoogle = (e) => {
     e.preventDefault();
@@ -16,145 +68,91 @@ function Login() {
     window.location.href = "http://localhost:8000/auth/facebook/redirect";
   };
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
-
-  const handleSumit = async (e) => {
-    e.preventDefault();
-    setError("");
-    console.log(email, password);
-    try {
-      const data = await callApi("auth/login", "POST", { email, password });
-      localStorage.setItem("token", data.access_token);
-      const userTypeFromSession = sessionStorage.getItem("type"); // Récupérer le type d'utilisateur de la session
-      if (userTypeFromSession === "fondateur") {
-        navigate("/publication");
-      } else if (userTypeFromSession === "investisseur") {
-        navigate("/publication");
-      } else {
-        navigate("/updateProfile");
-      }
-    } catch (err) {
-      console.error(err);
-      if (err.response && err.response.data && err.response.data.message) {
-        setError(err.response.data.message);
-      } else {
-        setError("Une erreur s'est produite. Veuillez réessayer.");
-      }
-    }
-  };
-
   return (
-    <>
-      <main>
-        {/* Container START */}
-        <div className="container">
-          <div className="row justify-content-center align-items-center vh-100 py-5">
-            {/* Main content START */}
-            <div className="col-sm-10 col-md-8 col-lg-7 col-xl-6 col-xxl-5">
-              {/* Sign in START */}
-              <div className="card card-body text-center p-4 p-sm-5">
-                {/* Title */}
-                <h1 className="mb-2">Connexion</h1>
-                {errorMessage && (
-                  <p className="text-danger mb-3">{errorMessage}</p>
-                )}
-
-                {/* Form START */}
-                <form className="mt-sm-4" onSubmit={handleSumit}>
-                  {/* Email */}
-                  <div className="mb-3 input-group-lg">
+    <main>
+      <div className="container">
+        <div className="row justify-content-center align-items-center vh-100 py-5">
+          <div className="col-sm-10 col-md-8 col-lg-7 col-xl-6 col-xxl-5">
+            <div className="card card-body text-center p-4 p-sm-5">
+              <h1 className="mb-2">Connexion</h1>
+              {error && <p className="text-danger mb-3">{error}</p>}
+              <form className="mt-sm-4" onSubmit={handleSumit}>
+                <div className="mb-3 input-group-lg">
+                  <input
+                    type="email"
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Entrez votre email"
+                  />
+                </div>
+                <div className="mb-3 position-relative">
+                  <div className="input-group input-group-lg">
                     <input
-                      type="email"
-                      className="form-control"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Entrez votre email"
+                      className="form-control fakepassword"
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Entrez votre mot de passe"
                     />
                   </div>
-                  {/* Nouveau mot de passe */}
-                  <div className="mb-3 position-relative">
-                    {/* Mot de passe */}
-                    <div className="input-group input-group-lg">
-                      <input
-                        className="form-control fakepassword"
-                        type="password"
-                        id="psw-input"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Entrez votre nouveau mot de passe"
-                      />
-                      <span className="input-group-text p-0">
-                        <i className="fakepasswordicon fa-solid fa-eye-slash cursor-pointer p-2 w-40px" />
-                      </span>
-                    </div>
+                </div>
+                <div className="mb-3 d-sm-flex justify-content-between">
+                  <div>
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="rememberCheck"
+                    />
+                    <label className="form-check-label" htmlFor="rememberCheck">
+                      Se souvenir de moi?
+                    </label>
                   </div>
-                  {/* Se souvenir de moi */}
-                  <div className="mb-3 d-sm-flex justify-content-between">
-                    <div>
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="rememberCheck"
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor="rememberCheck"
-                      >
-                        Se souvenir de moi?
-                      </label>
-                    </div>
-                    <Link to="/forgot-password">Mot de passe oublié?</Link>
-                  </div>
-                  {/* Bouton */}
-                  <div className="d-grid">
-                    <button type="submit" className="btn btn-lg btn-primary">
-                      Se connecter
-                    </button>
-                  </div>
-                  {error && <p style={{ color: "red" }}>{error}</p>}
-                  <br />
-                  <div class="text-center">
-                    <p>
-                      Vous n'avez pas de compte?{" "}
-                      <Link to="/register">S'inscrire</Link>
-                    </p>
-                    <p>ou connectez-vous avec:</p>
-                    <button
-                      type="button"
-                      class="btn btn-link btn-floating mx-1"
-                      onClick={handleFacebook}
-                    >
-                      <i class="fab fa-facebook-f"></i>
-                    </button>
-
-                    <button
-                      type="button"
-                      class="btn btn-link btn-floating mx-1"
-                      onClick={handleGoogle}
-                    >
-                      <i class="fab fa-google"></i>
-                    </button>
-
-                    <button
-                      type="button"
-                      class="btn btn-link btn-floating mx-1"
-                    >
-                      <i class="fab fa-linkedin-in"></i>
-                    </button>
-                  </div>
-                </form>
-              </div>
-              {/* Sign in START */}
+                  <Link to="/forgot-password">Mot de passe oublié?</Link>
+                </div>
+                <div className="d-grid">
+                  <button
+                    type="submit"
+                    className="btn btn-lg btn-primary"
+                    disabled={loading}
+                  >
+                    {loading ? "Connexion en cours..." : "Se connecter"}
+                  </button>
+                </div>
+                <br />
+                <div className="text-center">
+                  <p>
+                    Vous n'avez pas de compte?{" "}
+                    <Link to="/register">S'inscrire</Link>
+                  </p>
+                  <p>ou connectez-vous avec:</p>
+                  <button
+                    type="button"
+                    className="btn btn-link btn-floating mx-1"
+                    onClick={handleFacebook}
+                  >
+                    <i className="fab fa-facebook-f"></i>
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-link btn-floating mx-1"
+                    onClick={handleGoogle}
+                  >
+                    <i className="fab fa-google"></i>
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-link btn-floating mx-1"
+                  >
+                    <i className="fab fa-linkedin-in"></i>
+                  </button>
+                </div>
+              </form>
             </div>
-          </div>{" "}
-          {/* Row END */}
+          </div>
         </div>
-        {/* Container END */}
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
 
