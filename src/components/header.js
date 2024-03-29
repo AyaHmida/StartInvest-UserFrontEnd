c66df7e546de0983b123ba18c82da6597836df93
aya <ayahmida64@gmail.com>
2024-03-29 14:47:13 +0100

    logo

--- src/components/header.js
+++ src/components/header.js
@@ -52,7 +52,7 @@ function Header() {
       });
 
       setNotifications(uniqueNotifications);
-      console.log(uniqueNotifications);
+      // console.log(uniqueNotifications);
     });
   };
 
@@ -99,7 +99,7 @@ function Header() {
                 className="light-mode-item navbar-brand-item"
                 src="assets/images/logo.png"
                 alt="logo"
-                style={{ width: "150px", height: "auto" }} // Ajoutez cette ligne pour spécifier la taille du logo
+                style={{ width: "100px", height: "auto" }} // Ajoutez cette ligne pour spécifier la taille du logo
               />
               <img
                 className="dark-mode-item navbar-brand-item"
--- src/profile/publicationsProfile.js
+++ src/profile/publicationsProfile.js
@@ -134,7 +134,7 @@ const PublicationsProfile = () => {
               {item.file ? (
                 <div className="d-flex justify-content-between">
                   <div className="row g-3">
-                    <div className="col-6">
+                    <div className="col">
                       <a
                         className="h-100"
                         href={`http://127.0.0.1:8000/uploads/${item.file}`}
