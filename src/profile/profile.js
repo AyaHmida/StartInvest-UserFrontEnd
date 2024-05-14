84f4885f30aaa4834e3169c8592717de2da5ab87
aya <ayahmida64@gmail.com>
2024-05-14 14:21:58 +0100

    profile

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -188,7 +188,8 @@ const Profile = () => {
                       <ul className="list-inline mb-0 text-center text-sm-start mt-3 mt-sm-0">
                         <li className="list-inline-item">
                           <i className="bi bi-calendar2-plus me-1" /> account
-                          created on {formatDate(userdetail.created_at)}
+                          compte créé le
+                          {formatDate(userdetail.created_at)}
                         </li>
                       </ul>
                     )}
