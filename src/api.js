03b5e044e9fc3846b4fd6a6d9324622e8ddcef7b
Ben Hmida Aya <130604514+AyaHmida@users.noreply.github.com>
2026-04-05 15:19:13 +0200

    Change API base URL to production endpoint

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL = "https://startinvest-backend.up.railway.app/api";
 
 export const callApi = async (
   endpoint,
